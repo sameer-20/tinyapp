5aaf1b81fc74883b47d304260d179172fbf439a0
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-03 23:22:40 +0000

    Added random number generator function

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const express = require ('express');
 const app =  express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -29,6 +32,15 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req,res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -40,5 +52,10 @@ app.listen(PORT, () => {
 });
 
 
+function generateRandomString() {
+  let randomStr = Math.random().toString(36).slice(2,8); 
+  return randomStr;
+
+}
 
 
