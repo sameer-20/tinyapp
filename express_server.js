d6a546b855fe7f1b4e79520ba444931c54545654
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-03 21:02:18 +0000

    Changed longURL value in /urls/:shortURL section

--- express_server.js
+++ express_server.js
@@ -30,7 +30,7 @@ app.get("/urls", (req,res) => {
 });
 
 app.get("/urls/:shortURL", (req,res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
